6fd6bd5992d74c1addc6a82499cf695fd16b47a5
Cristina Leon <leogim_93@hotmail.com>
2023-02-24 18:49:02 -0500

    usuarios cuentas

--- usuarios_cuenta.py
+++ usuarios_cuenta.py
@@ -20,68 +20,55 @@ class CuentaBancaria:
         if self.account_balance >= saldo:
             self.account_balance -= saldo
         else:
-            self.account_balance -= 5
             print(
                 f"Fondos insuficientes: cobrando una tarifa de $5")
+            self.account_balance -= 5
         return self
 
     def generar_interes(self):
-        if self.account_balance >= 0:
+        if self.account_balance > 0:
             self.account_balance += self.account_balance * self.tasa_int
-        else:
-            self.account_balance
+        return self
+
+    def transferir_dinero(self, amount, user):
+        self.amount -= amount
+        user.amount += amount
+        self.mostrar_info_cuenta()
+        user.mostrar_info_cuenta()
         return self
 
     def mostrar_info_cuenta(self):
-        print(f"Balance Inicial: ${self.account_balance}")
-        return self
+        return f"{self.account_balance}"
 
     # Metodos de clase
     @classmethod
     def mostrar_cuentas(cls):
         for cuenta in cls.general_cuentas:
-            print('Balance general: $', cuenta.account_balance)
+            cuenta.mostrar_info_cuenta()
 
 
 class User:
-    all_users = []
-
-    def __init__(self, name, email):
+    def __init__(self, name):
         self.name = name
-        self.email = email
-        self.account = CuentaBancaria(tasa_int=0.01, account_balance=0)
-        User.all_users.append(self)
+        self.balance = {
+            "checking": CuentaBancaria(.01, 0),
+            "savings": CuentaBancaria(.02, 0)
+        }
 
-    @classmethod
-    def users(cls):
-        new_user = ""
-        for user in cls.all_users:
-            new_user = user + user.name + " -  "
-            return new_user[0: len(new_user) - 3]
-
-    def deposito(self):
-        self.account.deposito(1000)
-        return self
-
-    def retiro(self, amount):
-        self.account.retiro(200)
-        return self
-
-    def mostrar_balance(self):
-        print(f"Usuario: {self.name}, Total en cuenta: {self.account}")
+    def generar_balance(self):
+        print(
+            f"Usuario: {self.name}, Cuenta: Checking Balance: {self.balance['checking'].mostrar_info_cuenta()}")
+        print(
+            f"Usuario: {self.name}, Cuenta: Savings Balance: {self.balance['savings'].mostrar_info_cuenta()}")
         return self
 
 
-cristina = User("Cristina", "leonos@gmail.com")
-jorge = User("Jorge", "jorge@gmail.com")
+cristina = User("Cristina")
 
-Cuenta1 = CuentaBancaria(0.01, 0)
-Cuenta2 = CuentaBancaria(0.01, 0)
+cristina.balance["checking"].deposito(1000).deposito(20).deposito(
+    40).retiro(600).generar_interes().mostrar_info_cuenta()
+cristina.balance["savings"].deposito(1000).deposito(20).deposito(
+    40).retiro(600).generar_interes().mostrar_info_cuenta()
 
-Cuenta1.deposito(100).deposito(200).deposito(
-    150).retiro(100).generar_interes().mostrar_info_cuenta()
 
-Cuenta2.deposito(300).deposito(150).retiro(
-    100).retiro(200).retiro(100).retiro(60).generar_interes().mostrar_info_cuenta()
-
-CuentaBancaria.mostrar_cuentas()
+cristina.generar_balance()
